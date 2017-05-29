cd3517b1e20ca1a883f6ee1890ca379671dbe5ee
GotoFinal <bartlomiejkmazur@gmail.com>
2017-05-29 16:17:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
 unset TMUX
-tmux kill-session -t jekyll
+echo "Kill current session..."
+tmux kill-session -t jekyllserver
+echo "Run build"
 jekyll b
-tmux new-session -d -s jekyll
-tmux send-keys -t jekyll 'jekyll s' C-m
+echo "Create new session"
+tmux new-session -d -s jekyllserver
+echo "Start jekyll"
+tmux send-keys -t jekyllserver 'jekyll s' C-m
+echo "Start update loop"
 while [ "true" ]
 do
     gitout=$(git pull)
+	echo $gitout
     if ! [[ $gitout == *"Already up-to-date"* ]]; then
-        tmux kill-session -t jekyll
-        jekyll b
-        tmux new-session -d -s jekyll
-        tmux send-keys -t jekyll 'jekyll s' C-m
+        echo "Found update!"
+        jekyllserver b
+        tmux send-keys -t jekyllserver 'jekyll s' C-r
+        tmux send-keys -t jekyllserver 'jekyll s' C-m
     fi
     sleep 60
 done
