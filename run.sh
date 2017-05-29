edb4c41f4034ea5179584ebc628cc6f664945ec0
GotoFinal <bartlomiejkmazur@gmail.com>
2017-05-29 15:47:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-gitout=$(git pull)
-if [[ $gitout == *"Already up-to-date"* ]]; then
-    tmux kill-session -t jekyll
-    jekyll b
-    tmux new-session -d -s jekyll
-    tmux send-keys -t jekyll 'jekyll s' C-m
-fi
+while [ "true" ]
+do
+    gitout=$(git pull)
+    if ! [[ $gitout == *"Already up-to-date"* ]]; then
+        tmux kill-session -t jekyll
+        jekyll b
+        tmux new-session -d -s jekyll
+        tmux send-keys -t jekyll 'jekyll s' C-m
+    fi
+    sleep 60
+done
