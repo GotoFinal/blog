07976d50c9a150e5d6c72cfc59a2478f702c9faa
GotoFinal <bartlomiejkmazur@gmail.com>
2017-05-29 15:54:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+unset TMUX
 tmux kill-session -t jekyll
 jekyll b
 tmux new-session -d -s jekyll
 tmux send-keys -t jekyll 'jekyll s' C-m
-
 while [ "true" ]
 do
     gitout=$(git pull)
