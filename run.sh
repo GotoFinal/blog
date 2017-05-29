7b87a8807215512efcacc9258612a26170bb6845
GotoFinal <bartlomiejkmazur@gmail.com>
2017-05-29 15:40:31 +0200

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+gitout=$(git pull)
+if [[ $gitout == *"Already up-to-date"* ]]; then
+    tmux kill-session -t jekyll
+    jekyll b
+    tmux new-session -d -s jekyll
+    tmux send-keys -t jekyll 'jekyll s' C-m
+fi
