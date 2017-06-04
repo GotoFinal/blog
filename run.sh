fc8a0ac2201e5bdf180db67b7a29d86b1308f880
GotoFinal <bartlomiejkmazur@gmail.com>
2017-06-04 22:32:28 +0200

    trying to fix run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ unset TMUX
 echo "Kill current session..."
 tmux kill-session -t jekyllserver
 echo "Run build"
+jekyll clean
 jekyll b
 echo "Create new session"
 tmux new-session -d -s jekyllserver
@@ -16,8 +17,9 @@ do
 	echo $gitout
     if ! [[ $gitout == *"Already up-to-date"* ]]; then
         echo "Found update!"
+        jekyll clean
         jekyll b
-        tmux send-keys -t jekyllserver 'jekyll s' C-r
+        tmux send-keys -t jekyllserver C-c
         tmux send-keys -t jekyllserver 'jekyll s' C-m
     fi
     sleep 60
